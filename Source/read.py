dcb4f0b352bc9b995dc23485b1a7bc97304e0b1e
mohaoran <m.haoran@innopolis.ru>
2017-12-05 15:38:08 +0100

    init
    read and parse data

---
+++ Source/read.py
@@ -0,0 +1,15 @@
+# read the csv file
+import pandas as pd
+
+class GetData():
+    def read(self, name = None):
+        if name == None:
+            name = input("Type in the data set file name then continue, {bscale, car, kr-vs-kp}")
+        else:
+            pass
+        try:
+            df = pd.read_csv('Data/'+name+'.csv')
+            print('OK','I got the file: '+name+".csv")
+        except:
+            print("No this file")
+        return df
